613c9348264d2de3938132965da0c3531f387b9e
mariaestraviz <maria.estraviz.pardo@gmail.com>
2013-05-24 19:49:32 +0200

    mensajes flash

--- app.js
+++ app.js
@@ -28,8 +28,16 @@ app.configure(function(){
 	app.use(express.logger('dev'));
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
-	app.use(express.cookieParser('your secret here'));
+	app.use(express.cookieParser('--Core Blog 2012--'));
 	app.use(express.session());
+
+  app.use(require('connect-flash')());
+  // Hacer visible req.flash() en las vistas
+  app.use(function(req, res, next) {
+     res.locals.flash = function() { return req.flash() };
+     next();
+  });
+  
   app.use(counter.getContador);
 	app.use(app.router);
 	app.use(express.static(path.join(__dirname, 'public')));
--- routes/post_controller.js
+++ routes/post_controller.js
@@ -152,21 +152,29 @@ exports.new = function(req, res, next) {
 
 // POST /posts
 exports.create = function(req, res, next) {
-  var post = models.Post.build(
-    { title: req.body.post.title,
-      body: req.body.post.body,
-      authorId: 0
-    });
+    var post = models.Post.build(
+        { title: req.body.post.title,
+          body: req.body.post.body,
+          authorId: 0
+        });
     
     var validate_errors = post.validate();
     if (validate_errors) {
-        console.log("Errores de validacion:", validate_errors);
-        res.render('posts/new', {post: post});
+        console.log("Errores de validación:", validate_errors);
+
+        req.flash('error', 'Los datos del formulario son incorrectos.');
+        for (var err in validate_errors) {
+           req.flash('error', validate_errors[err]);
+        };
+
+        res.render('posts/new', {post: post,
+                                 validate_errors: validate_errors});
         return;
     }
     
     post.save()
         .success(function() {
+            req.flash('success', 'Post creado con éxito.');
             res.redirect('/posts');
         })
         .error(function(error) {
@@ -182,21 +190,29 @@ exports.edit = function(req, res, next) {
 // PUT /posts/33
 exports.update = function(req, res, next) {
   req.post.title = req.body.post.title;
-    req.post.body = req.body.post.body;
+  req.post.body = req.body.post.body;
                 
     var validate_errors = req.post.validate();
     if (validate_errors) {
-        console.log("Errores de validacion:", validate_errors);
-        res.render('posts/edit', {post: req.post});
+        console.log("Errores de validación:", validate_errors);
+
+        req.flash('error', 'Los datos del formulario son incorrectos.');
+        for (var err in validate_errors) {
+            req.flash('error', validate_errors[err]);
+        };
+
+        res.render('posts/edit', {post: req.post,
+                                  validate_errors: validate_errors});
         return;
     }
     req.post.save(['title', 'body'])
         .success(function() {
+            req.flash('success', 'Post actualizado con éxito.');
             res.redirect('/posts');
         })
         .error(function(error) {
             next(error);
-        });   
+        });
 };
 
 // DELETE /posts/33
